7526730053947a25db94151412f70f4531b3f4f2
CaigCaig <caig@inbox.ru>
2023-10-08 23:17:52 +0300

    Initial commit

---
+++ 2023-10-08_Task06_HW01_count_sym_sort.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+struct comp
+{
+	template<typename T>
+	bool operator()(const T& l, const T& r) const
+	{
+		if (l.second != r.second) {
+			return l.second > r.second;
+		}
+
+		return l.first > r.first;
+	}
+};
+
+int main() {
+
+	string str{ "Hello world!!" };
+	cout << "[IN]: " << str << endl << "[OUT]:" << endl;
+	map<char, int> m;
+
+	auto f_obj{ [&](char ch) { m[ch]++; } };
+
+	for_each(begin(str), end(str), f_obj);
+
+	set<pair<char, int>, comp> set(m.begin(), m.end());
+    for (const auto& p : set) cout << p.first << ": " << p.second << endl;
+
+	system("pause");
+	return 0;
+}
